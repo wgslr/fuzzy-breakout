e6c50cd11e4c87028d4d383aa4c384e9a228618b
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-21 21:53:58 +0200

    Add ball bouncing of the walls

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 'use strict';
 
 
-const CANVAS_HEIGHT = 500;
-const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 300;
+const CANVAS_WIDTH = 300;
 const CANVAS_ID = "canvas";
 
 const PLAYER_HEIGHT = 20;
 const PLAYER_WIDTH = 50;
 const PLAYER_ID = "player";
+
+const BALL_HEIGHT = 30;
+const BALL_WIDTH = 30;
+const BALL_ID = "ball";
 
 const TIMESTEP = 1000 / 60;
 
@@ -32,27 +36,51 @@ class Object {
     this.element.style.left = this.xl + 'px';
     this.element.style.bottom = this.yb + 'px';
   }
+
+  update(delta) {
+    this.xl += this.vx * delta;
+    this.yb += this.vy * delta;
+
+    if (this.xl <= 0) {
+      this.vx = Math.abs(this.vx);
+    } else if (this.xr >= CANVAS_WIDTH) {
+      this.vx = -Math.abs(this.vx);
+    }
+    if (this.yb <= 0) {
+      this.vy = Math.abs(this.vy);
+    } else if (this.yt >= CANVAS_HEIGHT) {
+      this.vy = -Math.abs(this.vy);
+    }
+  }
+
 }
 
 class Player extends Object {
   constructor() {
     super();
     this.element = document.getElementById(PLAYER_ID);
-    this.vx = 0.01;
+    this.vx = 0.05;
     this.width = PLAYER_WIDTH;
     this.height = PLAYER_HEIGHT;
   }
+}
 
-  update(delta) {
-    this.xl += this.vx * delta;
-    this.yb += this.vy * delta;
+
+class Ball extends Object {
+  constructor() {
+    super();
+    this.element = document.getElementById(BALL_ID);
+    this.vx = -0.05;
+    this.vy = -0.1;
+    this.width = BALL_WIDTH;
+    this.height = BALL_HEIGHT;
   }
-
 }
 
 class State {
   constructor() {
     this.player = new Player();
+    this.ball = new Ball();
   }
 
   initialDraw() {
@@ -61,14 +89,17 @@ class State {
     canvas.style.height = CANVAS_HEIGHT + 'px';
 
     this.player.initialDraw();
+    this.ball.initialDraw();
   }
 
   draw() {
     this.player.draw();
+    this.ball.draw();
   }
 
   update(delta) {
     this.player.update(delta);
+    this.ball.update(delta);
   }
 }
 
